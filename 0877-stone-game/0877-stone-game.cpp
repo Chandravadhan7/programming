98c2e110d2c1694436cfb628c9e864a13b1ff002
Chandravadhan7 <160453705+Chandravadhan7@users.noreply.github.com>
2026-08-02 21:24:57 +0530

    Time: 17 ms (16.3%), Space: 19.9 MB (7.37%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
+    int topdown(int i,int j,vector<vector<int>> &dp,vector<int> &piles){
+        if(i == j){
+            return piles[i];
+        }
+
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+
+        int maax = max(piles[i]+topdown(i+1,j,dp,piles),piles[j]+topdown(i,j-1,dp,piles));
+
+        return dp[i][j] = maax;
+    }
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int n =piles.size();
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+
+        return topdown(0,n-1,dp,piles)>=0;
     }
 };
