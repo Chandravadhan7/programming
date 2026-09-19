e389897a097be0792ab85029b27f3e45c59da195
Chandravadhan7 <160453705+Chandravadhan7@users.noreply.github.com>
2026-09-19 19:59:57 +0530

    Time: 121 ms (10.5%), Space: 96.8 MB (7.5%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -2,48 +2,41 @@ class Solution {
 public:
     int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
         unordered_map<int,set<int>> mp;
-
         for(auto x : reservedSeats){
             mp[x[0]].insert(x[1]);
         }
-        int ans = 0;
+
+        int ans = 2*(n-mp.size());
 
         for(auto x : mp){
-            auto st = x.second;
-        
-            bool left = true;
-            bool mid = true;
-            bool right = true;
-
+            bool k1 = true;
             for(int i=2;i<=5;i++){
-                if(st.count(i)){
-                    left = false;
+                if(x.second.count(i)){
+                    k1 = false;
                     break;
                 }
             }
-
+            bool k2 = true;
             for(int i=4;i<=7;i++){
-                if(st.count(i)){
-                    mid = false;
-                    break;
-                }
-            }
-            for(int i=6;i<=9;i++){
-                if(st.count(i)){
-                    right = false;
+                if(x.second.count(i)){
+                    k2 = false;
                     break;
                 }
             }
 
-            if(left && right){
-               ans += 2;
-            }else if(left || mid || right){
-                ans += 1;
+            bool k3 = true;
+            for(int i=6;i<=9;i++){
+                if(x.second.count(i)){
+                    k3 = false;
+                    break;
+                }
+            }
+            if(k1 && k3){
+                ans += 2;
+            }else if(k1 || k2 || k3){
+                ans++;
             }
         }
-
-        ans += (n-(int)mp.size())*2;
-
         return ans;
     }
 };
